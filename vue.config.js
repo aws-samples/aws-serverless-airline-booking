bff0e0f6595415e3aaef42edf0af8169acdef9bb
Heitor Lessa <lessa@amazon.com>
2019-05-13 16:34:07 +0100

    improv: change frontend from src to src/frontend

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   pluginOptions: {
     quasar: {
@@ -8,5 +10,17 @@ module.exports = {
   configureWebpack: {
     devtool: "source-map"
   },
-  transpileDependencies: [/[\\\/]node_modules[\\\/]quasar-framework[\\\/]/]
+  transpileDependencies: [/[\\\/]node_modules[\\\/]quasar-framework[\\\/]/],
+  chainWebpack: config => {
+    config
+      .entry("app")
+      .clear()
+      .add("./src/frontend/main.js")
+      .end();
+    config.resolve.alias.set("@", path.join(__dirname, "./src/frontend"));
+    config.resolve.alias.set(
+      "variables",
+      path.join(__dirname, "./src/frontend/styles/quasar.variables.styl")
+    );
+  }
 };
