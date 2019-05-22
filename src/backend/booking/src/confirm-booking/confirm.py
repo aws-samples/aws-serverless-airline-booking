2881d1b17f603ef5f63c54d0a5e1230642216849
Heitor Lessa <lessa@amazon.com>
2019-05-22 19:02:37 +0100

    improv: clean up confirmBooking function & use deployed table when prototyping

--- src/backend/booking/src/confirm-booking/confirm.py
+++ src/backend/booking/src/confirm-booking/confirm.py
@@ -4,42 +4,51 @@ import random
 import string
 
 import boto3
-from boto3.dynamodb.conditions import Attr
 from botocore.exceptions import ClientError
 
-error_string = '{{ "Status" : "Failed", "Error" : "{}" }}'
+session = boto3.Session()
+dynamodb = session.resource("dynamodb")
+table = dynamodb.Table(os.environ["BOOKING_TABLE_NAME"])
 
-def generate_booking_reference(length):
-    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
 
-def lambda_handler(event, context):
-    dynamodb = boto3.resource("dynamodb", region_name='eu-west-2')
-    table = dynamodb.Table(os.environ['BOOKING_TABLE_NAME'])
-    return_val = error_string.format('Unknown error.')
-
-    # Validate the input
-    try:
-        booking_id = event['bookingId']
-    except:
-        raise BookingConfirmationException("No booking_id provided.")
-    try:
-        # Just take the first one and write the redeemer to it.
-        reference = generate_booking_reference(6)
-        update_response = table.update_item(
-            Key={
-                'id': booking_id
-            },
-            ConditionExpression= 'id = :idVal',
-            UpdateExpression='set bookingReference = :br',
-            ExpressionAttributeValues={
-                ':br': reference,
-                ':idVal': booking_id
-            }
-        )
-        return_val = '{{ "Status" : "Success", "BookingReference" : "{}" }}'.format(reference)
-    except ClientError as e:
-        raise BookingConfirmationException(e.response['Error']['Message'])
-    return json.loads(return_val)
-    
 class BookingConfirmationException(Exception):
     pass
+
+
+def generate_booking_reference(length):
+    return "".join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
+
+def confirm_booking(booking_id):
+    try:
+        reference = generate_booking_reference(6)
+        table.update_item(
+            Key={"id": booking_id},
+            ConditionExpression="id = :idVal",
+            UpdateExpression="SET bookingReference = :br, #STATUS = :confirmed",
+            ExpressionAttributeNames={"#STATUS": "status"},
+            ExpressionAttributeValues={
+                ":br": reference,
+                ":idVal": booking_id,
+                ":confirmed": "CONFIRMED",
+            },
+        )
+
+        return {
+            "bookingReference": reference
+        }
+    except ClientError as e:
+        raise BookingConfirmationException(e.response["Error"]["Message"])
+
+
+def lambda_handler(event, context):
+
+    if "bookingId" not in event:
+        raise BookingConfirmationException("Invalid booking ID")
+
+    try:
+        ret = confirm_booking(event["bookingId"])
+    except BookingConfirmationException as e:
+        raise BookingConfirmationException(e)
+
+    return json.dumps(ret)
