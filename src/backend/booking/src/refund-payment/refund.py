66de450e639f6cd94533a77c344bb1fc0f6c5711
Heitor Lessa <lessa@amazon.com>
2019-05-24 16:38:07 +0100

    improv: cleans up refund payment, local prototyping helpers

--- src/backend/booking/src/refund-payment/refund.py
+++ src/backend/booking/src/refund-payment/refund.py
@@ -1,5 +1,11 @@
-from botocore.vendored import requests
+import json
 import os
+
+from botocore.vendored import requests
+
+# Payment API Refund URL to collect payment(i.e. https://endpoint/refund)
+payment_endpoint = os.environ["PAYMENT_API_URL"]
+
 
 class RefundException(Exception):
     def __init__(self, message, status_code):
@@ -10,15 +16,61 @@ class RefundException(Exception):
         # Now for your custom code...
         self.status_code = status_code
 
-def refund_payment(charge):
-    paymentApiResponse = requests.post(os.environ['PAYMENT_API_URL'], json = { "chargeId":charge["chargeId"]})
-    print(paymentApiResponse.content)
 
-    if paymentApiResponse.status_code == 200:
-        return { 
-            paymentApiResponse.content
-        }
-    raise RefundException('Refund failed.', paymentApiResponse.status_code)
+def refund_payment(charge_id):
+    """Refunds payment from a given charge ID through Payment API
+
+    For more info on Stripe Refund Object: https://stripe.com/docs/api/refunds/object
+
+    Parameters
+    ----------
+    charge_id : string
+        Pre-authorized charge ID received from Payment API
+
+    Returns
+    -------
+    dict
+        refundId: string
+    """
+    refund_payload = {"chargeId": charge_id}
+    ret = requests.post(payment_endpoint, json=refund_payload)
+    refund_response = ret.json()
+
+    if ret.status_code != 200:
+        print(refund_response)
+        raise RefundException("Refund failed", ret.status_code)
+
+    return {"refundId": refund_response["createdRefund"]["id"]}
+
 
 def lambda_handler(event, context):
-    refund_payment(event)
+    """AWS Lambda Function entrypoint to refund payment
+
+    Parameters
+    ----------
+    event: dict, required
+        Step Functions State Machine event
+
+        chargeId: string
+            pre-authorization charge ID
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    -------
+    string
+        JSON Stringified data containing refundId ID
+
+    Raises
+    ------
+    BookingConfirmationException
+        Booking Confirmation Exception including error message upon failure
+    """
+    if "chargeId" not in event:
+        raise RefundException(message="Invalid Charge ID", status_code=400)
+
+    ret = refund_payment(event["chargeId"])
+
+    return json.dumps(ret)
