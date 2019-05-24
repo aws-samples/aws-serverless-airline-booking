52862ed663195d3b702b11504f52621ba5ece2bd
Heitor Lessa <lessa@amazon.com>
2019-05-24 15:48:47 +0100

    improv: cleans up collect payment, local prototyping helpers

--- src/backend/booking/src/collect-payment/collect.py
+++ src/backend/booking/src/collect-payment/collect.py
@@ -1,21 +1,78 @@
-from botocore.vendored import requests
+import json
 import os
+
+from botocore.vendored import requests
+
+# Payment API Capture URL to collect payment(i.e. https://endpoint/capture)
+payment_endpoint = os.environ["PAYMENT_API_URL"]
+
 
 class PaymentException(Exception):
     def __init__(self, message, status_code):
 
-        # Call the base class constructor with the parameters it needs
         super(PaymentException, self).__init__(message)
-
-        # Now for your custom code...
         self.status_code = status_code
 
-def collect_payment(payment):
-    paymentApiResponse = requests.post(os.environ['PAYMENT_API_URL'], json = { "chargeId":payment["chargeId"]})
-    print(paymentApiResponse.content)
-    if paymentApiResponse.status_code == 200:
-        return paymentApiResponse.content
-    raise PaymentException('Payment failed', paymentApiResponse.status_code)
+
+def collect_payment(charge_id):
+    """Collects payment from a pre-authorized charge through Payment API
+
+    For more info on Stripe Charge Object: https://stripe.com/docs/api/charges/object
+
+    Parameters
+    ----------
+    charge_id : string
+        Pre-authorized charge ID received from Payment API        
+
+    Returns
+    -------
+    dict
+        chargeId: string
+        receiptUrl: string
+            receipt URL containing more details about the successful charge
+    """
+    payment_payload = {"chargeId": charge_id}
+    ret = requests.post(payment_endpoint, json=payment_payload)
+    payment_response = ret.json()
+
+    if ret.status_code != 200:
+        print(payment_response)
+        raise PaymentException("Payment failed", ret.status_code)
+
+    return {
+        "chargeId": charge_id,
+        "receiptUrl": payment_response["capturedCharge"]["receipt_url"],
+    }
+
 
 def lambda_handler(event, context):
-    collect_payment(event)
+    """AWS Lambda Function entrypoint to collect payment
+
+    Parameters
+    ----------
+    event: dict, required
+        Step Functions State Machine event
+
+        chargeId: string
+            pre-authorization charge ID
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    -------
+    string
+        JSON Stringified data containing charge ID and receipt URL
+
+    Raises
+    ------
+    BookingConfirmationException
+        Booking Confirmation Exception including error message upon failure
+    """
+    if "chargeId" not in event:
+        raise PaymentException(message="Invalid Charge ID", status_code=400)
+
+    ret = collect_payment(event["chargeId"])
+
+    return json.dumps(ret)
