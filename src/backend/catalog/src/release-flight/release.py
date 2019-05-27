fb04f761809ada510f484a2bc77c14d965cf8f23
Heitor Lessa <lessa@amazon.com>
2019-05-27 18:29:47 +0100

    feat: add initial Catalog SAM not yet deployed

--- src/backend/catalog/src/release-flight/release.py
+++ src/backend/catalog/src/release-flight/release.py
@@ -6,24 +6,26 @@ from botocore.exceptions import ClientError
 
 session = boto3.Session()
 dynamodb = session.resource('dynamodb')
-table = dynamodb.Table('flights')
-#table = dynamodb.Table(os.environ['FLIGHT_TABLE_NAME'])
+table = dynamodb.Table(os.environ['FLIGHT_TABLE_NAME'])
 
 
 class FlightReservationException(Exception):
     pass
 
+
 class FlightFullyBookedException(FlightReservationException):
     pass
+
 
 class FlightDoesNotExistException(FlightReservationException):
     pass
 
+
 def reserve_seat_on_flight(flight_id):
     try:
         # TODO: This needs to find the max. In theory, we should never have a situation
-        #       where we're trying to increment the seat when one hasn't been 
-        #       decremented, but just to be sure. 
+        #       where we're trying to increment the seat when one hasn't been
+        #       decremented, but just to be sure.
         table.update_item(
             Key={"id": flight_id},
             ConditionExpression="id = :idVal AND seatAllocation < maximumSeating",
@@ -37,19 +39,18 @@ def reserve_seat_on_flight(flight_id):
         return {
             'status': 'SUCCESS'
         }
-    #except dynamodb.meta.client.exceptions.
     except dynamodb.meta.client.exceptions.ConditionalCheckFailedException as e:
-        # Due to no specificity from the DDB error, this could also mean the flight 
+        # Due to no specificity from the DDB error, this could also mean the flight
         # doesn't exist, but we should've caught that earlier in the flow.
         # TODO: Fix that. Could either use TransactGetItems, or Get then Update.
-        raise FlightFullyBookedException(f"Flight with ID: {flight_id} is fully booked.") 
+        raise FlightFullyBookedException(f"Flight with ID: {flight_id} is fully booked.")
     except ClientError as e:
         raise FlightReservationException(e.response['Error']['Message'])
 
 
 def lambda_handler(event, context):
     if 'outboundFlightId' not in event:
-        raise FlightReservationException('Invalid arguments')
+        raise ValueError('Invalid arguments')
 
     try:
         ret = reserve_seat_on_flight(event['outboundFlightId'])
--- src/backend/catalog/src/reserve-flight/reserve.py
+++ src/backend/catalog/src/reserve-flight/reserve.py
@@ -12,11 +12,14 @@ table = dynamodb.Table(os.environ['FLIGHT_TABLE_NAME'])
 class FlightReservationException(Exception):
     pass
 
+
 class FlightFullyBookedException(FlightReservationException):
     pass
 
+
 class FlightDoesNotExistException(FlightReservationException):
     pass
+
 
 def reserve_seat_on_flight(flight_id):
     try:
@@ -35,17 +38,17 @@ def reserve_seat_on_flight(flight_id):
             'status': 'SUCCESS'
         }
     except dynamodb.meta.client.exceptions.ConditionalCheckFailedException as e:
-        # Due to no specificity from the DDB error, this could also mean the flight 
+        # Due to no specificity from the DDB error, this could also mean the flight
         # doesn't exist, but we should've caught that earlier in the flow.
         # TODO: Fix that. Could either use TransactGetItems, or Get then Update.
-        raise FlightFullyBookedException(f"Flight with ID: {flight_id} is fully booked.") 
+        raise FlightFullyBookedException(f"Flight with ID: {flight_id} is fully booked.")
     except ClientError as e:
         raise FlightReservationException(e.response['Error']['Message'])
 
 
 def lambda_handler(event, context):
     if 'outboundFlightId' not in event:
-        raise FlightReservationException('Invalid arguments')
+        raise ValueError('Invalid arguments')
 
     try:
         ret = reserve_seat_on_flight(event['outboundFlightId'])
