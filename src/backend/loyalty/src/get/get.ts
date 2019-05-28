d9f6da51ae183944680935f82e2bff9257e7500e
Simon Thulbourn <sthulb@users.noreply.github.com>
2019-05-28 15:07:57 +0100

    add levels

--- src/backend/loyalty/src/get/get.ts
+++ src/backend/loyalty/src/get/get.ts
@@ -4,6 +4,11 @@ import * as aws from 'aws-sdk';
 const tableName = process.env.TABLE_NAME;
 const client = new aws.DynamoDB.DocumentClient();
 
+interface Result {
+  Points: number;
+  level: string;
+}
+
 export const handler = async (event: APIGatewayEvent, context: APIGatewayEventRequestContext): Promise<APIGatewayProxyResult> => {
   if (!event.pathParameters || !event.pathParameters.user_id) {
     throw new Error('user_id not defined');
@@ -27,8 +32,30 @@ export const handler = async (event: APIGatewayEvent, context: APIGatewayEventRe
     }
   }, function(err , data) { zomg = data }).promise();
 
+  let points = 0;
+
+  for (let v of zomg.Items) {
+    points += v.points;
+  }
+
+  const result = {
+    points: points,
+    level: level(points)
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify(zomg)
+    body: JSON.stringify(result as Object)
   };
+}
+
+const level = (points: number): string => {
+  switch (true) {
+    case (points > 100000):
+      return "gold";
+    case (points > 50000 && points < 100000):
+      return "silver"
+    default:
+      return "bronze";
+  } 
 }
