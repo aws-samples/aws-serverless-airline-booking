c4420960f64dc80ae14474e0c993df7e83cae782
Heitor Lessa <lessa@amazon.com>
2019-06-26 18:32:03 +0100

    improv: use command to abstract login functionality

--- src/e2e-tests/ui/cypress/integration/search_flights.spec.js
+++ src/e2e-tests/ui/cypress/integration/search_flights.spec.js
@@ -6,26 +6,14 @@ context('Search for flights', function () {
             arrivalCode: "MAD"
         }
 
-        // TODO: Move to fixture/support
-        cy.visit('/')
-        cy.get('.authenticator__form').within(() => {
-            cy.get('input:first').type("demo")
-            cy.get('input:last').type("Demo123!")
-
-            cy.get('button').click()
-
-        })
-        cy.get('.cta__button > .q-btn-inner > div').contains("Search flights")
-
-        // cy.login()
+        cy.login()
 
         // Search for a flight from London Gatwich to Madrid on June 26th 2019
         cy.visit('/#/search/results?date=2019-06-26&departure=LGW&arrival=MAD')
         cy.get(".flight__departure").contains(flight.departureCode)
         cy.get(".flight__arrival").contains(flight.arrivalCode)
-        // cy.get(".flight__price").contains("100 eur")
 
-        // Select 
+        // Select
         cy.get('.flight__card').trigger('mouseover').click()
     })
 })
