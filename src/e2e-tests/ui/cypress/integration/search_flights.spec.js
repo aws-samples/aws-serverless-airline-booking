73faaf62f403af096e9ce924ffd36a182e3afa00
Heitor Lessa <lessa@amazon.com>
2019-06-26 13:40:54 +0100

    improv: test whether flight was found

--- src/e2e-tests/ui/cypress/integration/search_flights.spec.js
+++ src/e2e-tests/ui/cypress/integration/search_flights.spec.js
@@ -1,6 +1,11 @@
 context('Search for flights', function () {
 
-    it('Search for flights', function () {
+    it('Search and select a flight', function () {
+        let flight = {
+            departureCode: "LGW",
+            arrivalCode: "MAD"
+        }
+
         // TODO: Move to fixture/support
         cy.visit('/')
         cy.get('.authenticator__form').within(() => {
@@ -8,9 +13,17 @@ context('Search for flights', function () {
             cy.get('input:last').type("Demo123!")
 
             cy.get('button').click()
+
         })
+        cy.get('.cta__button > .q-btn-inner > div').contains("Search flights")
 
-        // TODO: Search for a flight from LGW to MAD
+        // Search for a flight from London Gatwich to Madrid on June 26th 2019
+        cy.visit('/#/search/results?date=2019-06-26&departure=LGW&arrival=MAD')
+        cy.get(".flight__departure").contains(flight.departureCode)
+        cy.get(".flight__arrival").contains(flight.arrivalCode)
+        // cy.get(".flight__price").contains("100 eur")
 
+        // Select 
+        cy.get('.flight__card').trigger('mouseover').click()
     })
 })
