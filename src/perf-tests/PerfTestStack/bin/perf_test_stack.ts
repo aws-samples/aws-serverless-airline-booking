cb92257788a4adb0c7366c146d42c655c4184a26
Pawan Puthran <pputhran@amazon.com>
2019-09-19 17:53:53 +0100

    updated cdk ENV variables in bin

--- src/perf-tests/PerfTestStack/bin/perf_test_stack.ts
+++ src/perf-tests/PerfTestStack/bin/perf_test_stack.ts
@@ -2,7 +2,7 @@
 import cdk = require('@aws-cdk/core');
 import { PerfTestStackStack } from '../lib/perf_test_stack-stack';
 
-const STACK_NAME = process.env.STACK_NAME
+const STACK_NAME = process.env.PERF_TEST_STACK_NAME
 
 const app = new cdk.App();
 new PerfTestStackStack(app, 'PerfTestStackStack', {stackName: STACK_NAME});
