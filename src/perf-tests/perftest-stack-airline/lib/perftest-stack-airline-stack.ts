2e8548304a59347545be0b6d2182c3cba9e8f9ce
Pawan Puthran <pputhran@amazon.com>
2019-11-13 16:44:46 +0000

    testing userpool arn

--- src/perf-tests/perftest-stack-airline/lib/perftest-stack-airline-stack.ts
+++ src/perf-tests/perftest-stack-airline/lib/perftest-stack-airline-stack.ts
@@ -1,7 +1,18 @@
 import cdk = require('@aws-cdk/core');
 
+const COGNITO_USER_POOL_ARN = process.env.COGNITO_USER_POOL_ARN;
+
 export class PerftestStackAirlineStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    console.log(`COGNITO_USER_POOL_ARN - ${COGNITO_USER_POOL_ARN}`);
+
+    const pool = this.node.tryGetContext('id')
+
+    new cdk.CfnOutput(this, 'cognito-arn', {
+      value: pool.toString()
+    });
+
   }
 }
