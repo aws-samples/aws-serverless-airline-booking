6706eb6c266d1b885a59a6376a66b50d48cb9b58
heitorlessa <lessa@amazon.co.uk>
2020-02-06 11:06:12 +0000

    fix(eslint): disable console.log rule

--- src/frontend/.eslintrc.js
+++ src/frontend/.eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-console": "off"
   },
   parserOptions: {
     parser: "babel-eslint"
