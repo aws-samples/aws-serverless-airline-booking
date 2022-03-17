0c53f9f1b38795b81fc81e40b6811cf13d496aa0
Roman Boiko <roman.boiko@gmail.com>
2022-03-17 14:09:09 +0100

    fixed auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awsserverlessairlineb7722ff6": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/boot/amplify.js
+++ src/boot/amplify.js
@@ -1,8 +1,9 @@
 import Amplify from 'aws-amplify'
+import { Auth } from '@aws-amplify/auth'
 import awsconfig from '../aws-exports'
 import '@aws-amplify/ui-vue'
 
 const logLevel = process.env.LOG_LEVEL || 'INFO'
 Amplify.configure(awsconfig)
-
+Auth.configure(awsconfig)
 Amplify.Logger.LOG_LEVEL = logLevel
