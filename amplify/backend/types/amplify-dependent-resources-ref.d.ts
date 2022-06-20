8bb9ea25e73c9cd9f830a8837bf3bed4f336a72a
Roman Boiko <roman.boiko@gmail.com>
2022-06-20 09:44:10 +0200

    fix(workshop): upgrade to latest amplify, syntax, files (#198)

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awsserverlessairlineb7722ff6": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
