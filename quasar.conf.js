43ebabeb120cb6631109981ac29ff447195106f0
Simon Thulbourn <sthulb@users.noreply.github.com>
2022-11-30 11:17:00 +0100

    add dist dir

--- quasar.conf.js
+++ quasar.conf.js
@@ -56,6 +56,8 @@ module.exports = function (/* ctx */) {
       // Options below are automatically set depending on the env, set them if you want to override
       // extractCSS: false,
 
+      distDir: "./dist/",
+
       // https://quasar.dev/quasar-cli/handling-webpack
       extendWebpack(cfg) {
         cfg.module.rules.push({
